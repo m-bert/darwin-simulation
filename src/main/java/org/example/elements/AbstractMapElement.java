08bb04818b0c1d1b14f91b18f7560614fb17afe7
Kacper0199 <kacperkozik999@gmail.com>
2022-12-28 13:34:46 +0100

    add Animal, IMapElement, AbstractMapElement

--- src/main/java/org/example/elements/AbstractMapElement.java
+++ src/main/java/org/example/elements/AbstractMapElement.java
@@ -1,2 +1,22 @@
-package org.example.elements;public class AbstractMapElement {
+package org.example.elements;
+
+import org.example.utils.Vector2D;
+
+public class AbstractMapElement implements IMapElement{
+
+    protected Vector2D position;
+
+    public AbstractMapElement(Vector2D position) {
+        this.position = position;
+    }
+
+    @Override
+    public Vector2D getPosition() {
+        return position;
+    }
+
+    @Override
+    public String getPath() {
+        return null;
+    }
 }
---
+++ src/main/java/org/example/elements/Animal.java
@@ -0,0 +1,12 @@
+package org.example.elements;
+
+import org.example.utils.Vector2D;
+
+import java.util.Map;
+
+public class Animal extends AbstractMapElement{
+
+    public Animal(Vector2D position) {
+        super(position);
+    }
+}
