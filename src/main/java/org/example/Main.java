ca3a8e0974aaba47274c741d352c01fe3773fd72
Kacper0199 <kacperkozik999@gmail.com>
2022-12-29 15:51:21 +0100

    add some resources

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 import javafx.application.Application;
 import org.example.gui.App;
+import org.example.utils.MapDirection;
 
 public class Main {
     public static void main(String[] args) {
