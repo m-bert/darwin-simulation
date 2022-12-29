0192fd2f80ebea576c4204b49c066c26421469ec
Michał <misiek.b3870@gmail.com>
2022-12-29 12:56:05 +0100

    Working button

--- src/main/java/org/example/gui/App.java
+++ src/main/java/org/example/gui/App.java
@@ -1,2 +1,19 @@
-package org.example.gui;public class App {
+package org.example.gui;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.example.gui.controllers.SettingsController;
+
+public class App extends Application {
+    private Stage window;
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        window = primaryStage;
+
+        SettingsController settingsController = new SettingsController();
+        Scene settingsScene = new Scene(settingsController);
+        window.setScene(settingsScene);
+        window.show();
+    }
 }
---
+++ src/main/java/org/example/gui/controllers/SettingsController.java
@@ -0,0 +1,46 @@
+package org.example.gui.controllers;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.GridPane;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+
+public class SettingsController extends VBox {
+
+    @FXML private RadioButton globeRadio;
+    @FXML private RadioButton hellPortalRadio;
+    @FXML private RadioButton grassFieldRadio;
+    @FXML private RadioButton toxicCorpseRadio;
+    @FXML private RadioButton randomRadio;
+    @FXML private RadioButton correctionRadio;
+    @FXML private RadioButton predestinationRadio;
+    @FXML private RadioButton madnessRadio;
+
+
+    @FXML private Button startButton;
+
+    public SettingsController() {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/settings.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+
+    @FXML
+    public void startSimulation(){
+        System.out.println(globeRadio.isSelected());
+        System.out.println(hellPortalRadio.isSelected());
+    }
+}
