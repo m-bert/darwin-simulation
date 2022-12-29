cadac21274fced34115731d57cec1278fec3bfd1
Michał Bert <63123542+Warus15@users.noreply.github.com>
2022-12-29 13:56:26 +0100

    Basic JFX structure (#5)
    Basic JFX settings form

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import javafx.application.Application;
+import org.example.gui.App;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Application.launch(App.class, args);
     }
 }
